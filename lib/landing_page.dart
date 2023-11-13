8a49aa12ff15031d8fe304a3a801f3a08107bc1e
Sabilla Luthfi Rahmadhan <mamadhan017@gmail.com>
2023-11-13 15:37:58 +0700

    make a landing page

---
+++ lib/landing_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Container 1
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              width: 380,
+              child: Image.asset('../assets/images/logo.png'),
+            ),
+
+            // Container 2
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20),
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Add functionality for login button
+                      // Navigator.pushNamed(context, '/login'); // Example navigation
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          Colors.red, // Change the background color to red
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.24),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 14,
+                          color: Colors.white, // Change the text color to white
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Belum punya akun?',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontFamily: 'Poppins',
+                      fontSize: 12,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  InkWell(
+                    onTap: () {
+                      // Add functionality for Daftar
+                      // Navigator.pushNamed(context, '/daftar'); // Example navigation
+                    },
+                    child: Text(
+                      'Daftar',
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontFamily: 'Poppins',
+                        fontSize: 14,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // Container 3
+            Container(
+              margin: EdgeInsets.only(top: 40),
+              child: Text(
+                'Copyright ©2023 All rights reserved | Transrent.id',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontFamily: 'Poppins',
+                  fontSize: 10,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
