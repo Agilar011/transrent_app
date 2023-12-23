bf5e55e76ad20f76b887632ed8feb16d76a58680
Agilar011 <88954314+Agilar011@users.noreply.github.com>
2023-12-23 21:11:58 +0700

    update transaction

--- lib/transaction_page.dart
+++ lib/transaction_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart ';
 import 'package:transrent_app/profil_page.dart';
+import 'package:transrent_app/product_page.dart';
 
 class TransactionPage extends StatelessWidget {
   @override
@@ -52,12 +53,27 @@ class TransactionPage extends StatelessWidget {
                 const SizedBox(height: 10),
 
                 // Example Group Container (Replace with your dynamic data)
-                buildHistoryGroup(
+                buildHistoryGroupButton(
                   imageUrl:
                       'https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2023/06/07/Honda-BeAT-Sporty-2023-4025834754.jpg',
                   amount: 'Rp 30.000',
                   status: 'Sedang dilakukan',
                   title: 'Beat 2023',
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailProduct(
+                          imageUrl:
+                              'https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2023/06/07/Honda-BeAT-Sporty-2023-4025834754.jpg',
+                          title: 'Beat 2023',
+                          price: 'Rp 30.000',
+                          description:
+                              'Skutik 110cc dari honda yang keluar pada tahun 2023. Motor ini merupakan motor yang sesuai untuk medan perkotaan dengan pace yang cukup kencang. Motor ini juga memiliki fitur yang cukup lengkap untuk motor skutik.',
+                        ),
+                      ),
+                    );
+                  },
                 ),
                 // Add more groups based on your data
                 // ... (Add more group containers based on your data)
@@ -119,82 +135,87 @@ class TransactionPage extends StatelessWidget {
   }
 
   // Function to build a history group container
-  Widget buildHistoryGroup(
-      {String? imageUrl, String? amount, String? status, String? title}) {
-    return Container(
-      width: double.infinity,
-      height: 100,
-      margin: const EdgeInsets.only(bottom: 15),
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 1,
-            blurRadius: 5,
-            offset: const Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Row(
-        children: [
-          Container(
-            width: 66,
-            height: 62,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              image: DecorationImage(
-                image: NetworkImage(imageUrl!),
-                fit: BoxFit.fill,
+  Widget buildHistoryGroupButton({
+    required String imageUrl,
+    required String amount,
+    required String status,
+    required String title,
+    required VoidCallback onTap,
+  }) {
+    return InkWell(
+        onTap: onTap,
+        child: Container(
+          height: 100,
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 5,
+                offset: const Offset(0, 2),
               ),
-            ),
+            ],
           ),
-          const SizedBox(width: 15),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  title!,
-                  style: const TextStyle(
-                    color: Color(0xFFA51111),
-                    fontSize: 20,
-                    fontFamily: 'Work Sans',
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 0.40,
+          child: Row(
+            children: [
+              Container(
+                width: 66,
+                height: 62,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                    image: NetworkImage(imageUrl!),
+                    fit: BoxFit.fill,
                   ),
                 ),
-                const SizedBox(height: 5),
-                Text(
-                  status!,
-                  style: const TextStyle(
-                    color: Color(0xFFDF6F1D),
-                    fontSize: 12,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 0.40,
-                  ),
+              ),
+              const SizedBox(width: 15),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      title!,
+                      style: const TextStyle(
+                        color: Color(0xFFA51111),
+                        fontSize: 20,
+                        fontFamily: 'Work Sans',
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 0.40,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      status!,
+                      style: const TextStyle(
+                        color: Color(0xFFDF6F1D),
+                        fontSize: 12,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 0.40,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              Text(
+                amount!,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontFamily: 'Work Sans',
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 0.40,
+                ),
+              ),
+            ],
           ),
-          Text(
-            amount!,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              color: Colors.black,
-              fontSize: 16,
-              fontFamily: 'Work Sans',
-              fontWeight: FontWeight.w500,
-              letterSpacing: 0.40,
-            ),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 
   // Function to build a navigation button
